3b435770b2fcd5a962e7b252393945f0881295ef
Roberto Bonilla <croberto.bs@gmail.com>
2021-05-23 21:46:31 -0600

    Agregando Archivos

---
+++ App/WebApp/WebApp.Api/Controllers/ListaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ListaController : ControllerBase
+    {
+        public ListaController()
+        {
+
+        }
+        [HttpGet("GetLista")]
+        public string GetLista()
+        {
+            return "Hola Mundo";
+        }
+    }
+}
